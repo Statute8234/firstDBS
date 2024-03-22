78ddac324dc316d67e69b71cec5f72c77db19b1e
Statute8234 <76547158+Statute8234@users.noreply.github.com>
2024-03-21 20:47:04 -0400

    Update Inserting Data.sql

--- Inserting Data.sql
+++ Inserting Data.sql
@@ -6,8 +6,7 @@ CREATE TABLE student (
     PRIMARY KEY(student_id)
 );
 
-
 SELECT * FROM student;
-INSERT INTO student(student_id, name) VALUES(3, 'Sam');
-
-DESCRIBE student;
+INSERT INTO student VALUES(1, 'Sam', 'CIS');
+INSERT INTO student(student_id, name) VALUES(2, 'Mat');
+INSERT INTO student VALUES(3, 'David', 'Media');
